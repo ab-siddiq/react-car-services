a6bff0524fcdb6aaa25c41e19312c1194c4680f8
siddiq <siddiq.aiub@gmail.com>
2022-04-11 11:02:15 +0600

    initial project file

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOQ9HmIAe35wdkK3KfeU8P0SObVy1OzFw",
+  authDomain: "react-car-services.firebaseapp.com",
+  projectId: "react-car-services",
+  storageBucket: "react-car-services.appspot.com",
+  messagingSenderId: "654860593941",
+  appId: "1:654860593941:web:0f1ecefb4eeddfdd15171c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
