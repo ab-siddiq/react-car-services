2d2e4f6f8ef421a28e9a8eb438094b6d9294bd74
siddiq <siddiq.aiub@gmail.com>
2022-04-11 11:20:52 +0600

    create components, install router

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            
+            <p>&copy; {new Date().getFullYear()}</p>
+            <p>All right reserve Abu Bakkar Siddiq</p>
+            
+        </div>
+    );
+};
+
+export default Footer;
